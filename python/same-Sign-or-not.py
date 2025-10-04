a60edc9e1b568f2164ec42d76cc4b3ae23c8e7a5
BOUDKHOUM-ABDEL <boudkhoum.abdellah.code@gmail.com>
2025-10-04 22:52:03 +0100

    problem 4

---
+++ python/same-Sign-or-not.py
@@ -0,0 +1,5 @@
+
+number1=float(input("Enter first number: "))
+number2=float(input("Enter second number: "))
+
+print("Both numbers have the same sign." if number1*number2 >= 0 else "Both numbers have not the same sign.")
