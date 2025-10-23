13115c31532541bfa9e3568ceca56a73c62e49be
BOUDKHOUM-ABDEL <boudkhoum.abdellah.code@gmail.com>
2025-10-23 16:02:29 +0100

    update

--- python/same-Sign-or-not.py
+++
@@ -1,5 +0,0 @@
-
-number1=float(input("Enter first number: "))
-number2=float(input("Enter second number: "))
-
-print("Both numbers have the same sign." if number1*number2 >= 0 else "Both numbers have not the same sign.")
