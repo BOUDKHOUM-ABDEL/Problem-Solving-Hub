3171c9d0b792e09c9d00b12efae29c6729cbbf94
BOUDKHOUM-ABDEL <boudkhoum.abdellah.code@gmail.com>
2025-10-25 17:20:27 +0100

    update

--- python/TD/TD1/exercice1.py
+++ python/TD/TD1/exercice1.py
@@ -3,16 +3,16 @@
 # 1. Crée une liste de nombres entiers saisis par l’utilisateur. 
 
 my_list = []
-lenth = 5
-for index in range(lenth):
+length = 5
+for index in range(length):
     number = int(input(f"Enter the {index+1}th number : "))
     my_list.append(number)
 print(my_list)
     
 # 2. Affiche : 
 # o Liste triée sans utiliser sort() ni sorted():
-for i in range(lenth):
-    for j in range(i+1,lenth):
+for i in range(length):
+    for j in range(i+1,length):
         if my_list[i] > my_list[j]:
             temp = my_list[j]
             my_list[j] = my_list[i]
@@ -20,7 +20,7 @@ for i in range(lenth):
 print(my_list)
     
 # o Deuxième plus grand élément:
-index =lenth -1
+index =length -1
 if my_list[0] == my_list[index]:
     print("not found, all numbers are simillar")
 
@@ -29,6 +29,21 @@ else:
     index=index-1
    print(my_list[index-1])
 
-# o Somme des éléments aux indices pairs, 
-# o Nouvelle liste contenant uniquement les valeurs uniques (sans utiliser set()). 
-# 3. Remplace dans la liste tous les éléments négatifs par 0. 
+# o Somme des éléments aux indices pairs:
+total = 0
+for index in range(0,length,2):
+        total+=my_list[index]
+print(f"1.la Somme des elements aux indices pairs  :{total}")
+
+
+# o Nouvelle liste contenant uniquement les valeurs uniques (sans utiliser set()):
+unique_list = []
+for number in my_list:
+    if not number in unique_list:
+       unique_list.append(number)
+print(unique_list)
+# 3. Remplace dans la liste tous les éléments négatifs par 0:
+for index in range(length):
+    if my_list[index] < 0 :
+      my_list[index] = 0
+print(my_list)
